349b4b125e50d9f417289b3c2b466ec534e1c3b2
Jan Bodnar <jan.bodnar@gmail.com>
2020-10-12 12:04:38 +0200

    Create fetch_one.py

---
+++ fetch_one.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python
+
+import sqlite3
+
+con = sqlite3.connect('ydb.db')
+
+with con:
+
+    cur = con.cursor()
+    cur.execute("SELECT * FROM cars")
+
+    while True:
+
+        row = cur.fetchone()
+
+        if row == None:
+            break
+
+        print(f"{row[0]} {row[1]} {row[2]}")
